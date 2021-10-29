e9db8737b546c9b94964cc5f5fc044bdffda320d
divyachilakalapalli <44266006+divyachilakalapalli@users.noreply.github.com>
2021-10-29 16:31:05 +0530

    Update broken_links.py

--- broken_links.py
+++ broken_links.py
@@ -1,7 +1,6 @@
 import requests
 from selenium import webdriver
 import time
-where is it
 driver=webdriver.Chrome("/home/divya/Desktop/Python/webdriver/chromedriver")
 driver.get('http://www.irctc.com/')
 
